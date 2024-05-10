74f56bf3854c65e1c7b93f3740cd770851e60f45
kura1aym <mukhtarkuralay249@mail.ru>
2024-05-11 00:51:08 +0500

    add QuizApplication

---
+++ app/src/main/java/dev/androidbroadcast/quizapp/QuizApplication.kt
@@ -0,0 +1,11 @@
+package dev.androidbroadcast.quizapp
+
+import android.app.Application
+import com.google.android.material.color.DynamicColors
+
+class QuizApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
