205e6660f0d6d58c102160752cfbfcb94840f104
kura1aym <mukhtarkuralay249@mail.ru>
2024-05-11 00:52:00 +0500

    fix error with adding data from API

--- app/src/main/java/dev/androidbroadcast/quizapp/data/repository/QuizRepository.kt
+++ app/src/main/java/dev/androidbroadcast/quizapp/data/repository/QuizRepository.kt
@@ -1,41 +1,49 @@
 package dev.androidbroadcast.quizapp.data.repository
 
+import android.app.Application
 import android.util.Log
 import dev.androidbroadcast.quizapp.data.database.QuizDao
+import dev.androidbroadcast.quizapp.data.database.QuizRoomDatabase
 import dev.androidbroadcast.quizapp.data.mapper.mapApiResponseToQuiz
 import dev.androidbroadcast.quizapp.data.model.Leaderboard
 import dev.androidbroadcast.quizapp.data.model.Quiz
-import dev.androidbroadcast.quizapp.data.network.QuizApiService
+import dev.androidbroadcast.quizapp.data.network.ApiClient.quizApiService
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
-class QuizRepository(private val quizDao: QuizDao, private val quizApiService: QuizApiService) {
-
+class QuizRepository (application: Application) {
+    private val mQuizzesDao: QuizDao
     private val executorService: ExecutorService = Executors.newSingleThreadExecutor()
 
+    init {
+        val db = QuizRoomDatabase.getDatabase(application)
+        mQuizzesDao = db.quizDao()
+    }
+
     fun insert(quizData: Quiz) {
-        executorService.execute { quizDao.insert(quizData) }
+        executorService.execute { mQuizzesDao.insert(quizData) }
     }
 
     fun update(quizData: Quiz) {
-        executorService.execute { quizDao.update(quizData) }
+        executorService.execute { mQuizzesDao.update(quizData) }
     }
 
     fun delete(quizData: Quiz) {
-        executorService.execute { quizDao.delete(quizData) }
+        executorService.execute { mQuizzesDao.delete(quizData) }
     }
 
-    suspend fun getQuizData(): List<Quiz> = quizDao.getQuizData()
+    suspend fun getQuizData(): List<Quiz> = mQuizzesDao.getQuizData()
 
     fun insert(leaderboardData: Leaderboard) {
-        executorService.execute { quizDao.insert(leaderboardData) }
+        executorService.execute { mQuizzesDao.insert(leaderboardData) }
     }
 
     fun clearLeaderboardData() {
-        executorService.execute { quizDao.clearLeaderboardData() }
+        executorService.execute { mQuizzesDao.clearLeaderboardData() }
     }
 
-    suspend fun getLeaderboardData(): List<Leaderboard> = quizDao.getLeaderboardData()
+    suspend fun getLeaderboardData(): List<Leaderboard> = mQuizzesDao.getLeaderboardData()
+
 
     suspend fun fetchAndStoreQuizzes() {
         val response = quizApiService.fetchQuestions()
@@ -43,7 +51,7 @@ class QuizRepository(private val quizDao: QuizDao, private val quizApiService: Q
             val apiResponse = response.body()
             if (apiResponse != null) {
                 val quiz = mapApiResponseToQuiz(apiResponse)
-                quizDao.insert(quiz)
+                mQuizzesDao.insert(quiz)
             } else {
                 Log.e("QuizRepository", "Response body is null")
             }
@@ -51,7 +59,7 @@ class QuizRepository(private val quizDao: QuizDao, private val quizApiService: Q
             Log.e("QuizRepository", "Error: ${response.errorBody()?.string()}")
         }
     }
-}
 
+}
 
 
---
+++ app/src/main/java/dev/androidbroadcast/quizapp/ui/helper/ViewModelFactory.kt
@@ -0,0 +1,38 @@
+package dev.androidbroadcast.quizapp.ui.helper
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import dev.androidbroadcast.quizapp.ui.edit.viewmodel.EditQuestionViewModel
+import dev.androidbroadcast.quizapp.ui.main.viewmodel.MainViewModel
+import dev.androidbroadcast.quizapp.ui.play.viewmodel.PlayViewModel
+
+class ViewModelFactory(private val mApplication: Application) :
+    ViewModelProvider.NewInstanceFactory() {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            return MainViewModel(mApplication) as T
+        } else if (modelClass.isAssignableFrom(EditQuestionViewModel::class.java)) {
+            return EditQuestionViewModel(mApplication) as T
+        } else if (modelClass.isAssignableFrom(PlayViewModel::class.java)) {
+            return PlayViewModel(mApplication) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+    }
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ViewModelFactory? = null
+
+        @JvmStatic
+        fun getInstance(application: Application): ViewModelFactory {
+            if (INSTANCE == null) {
+                synchronized(ViewModelFactory::class.java) {
+                    INSTANCE = ViewModelFactory(application)
+                }
+            }
+            return INSTANCE as ViewModelFactory
+        }
+    }
+}
