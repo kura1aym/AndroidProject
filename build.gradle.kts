0773907e8b3670365749504035bf5eadc3378867
kura1aym <mukhtarkuralay249@mail.ru>
2024-05-10 16:10:37 +0500

    add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,14 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
+}
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.android.gms:oss-licenses-plugin:0.10.6")
+    }
 }
