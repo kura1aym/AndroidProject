736596f17bdca46667f99dcd0912d411a4c0af2d
Moldir <moldir.andirbekova@mail.ru>
2024-05-11 17:59:31 +0500

    Added Multiple Choice adapter and fragment

---
+++ app/src/main/java/dev/androidbroadcast/quizapp/ui/play/adapter/MultipleChoiceVariantAdapter.kt
@@ -0,0 +1,59 @@
+package dev.androidbroadcast.quizapp.ui.play.adapter
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import dev.androidbroadcast.quizapp.data.model.Answer
+import dev.androidbroadcast.quizapp.databinding.CheckboxRowItemBinding
+
+class MultipleChoiceVariantAdapter :
+    RecyclerView.Adapter<MultipleChoiceVariantAdapter.MultipleChoiceVariantViewHolder>() {
+    private val listAnswer = ArrayList<Answer>()
+    private var selectedItems = intArrayOf()
+
+    fun setListAnswer(listAnswer: List<Answer>) {
+        this.listAnswer.addAll(listAnswer)
+    }
+
+    fun getSelected(): IntArray {
+        return selectedItems
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MultipleChoiceVariantViewHolder {
+        val binding =
+            CheckboxRowItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MultipleChoiceVariantViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(
+        holder: MultipleChoiceVariantViewHolder,
+        @SuppressLint("RecyclerView") position: Int
+    ) {
+        holder.bind(listAnswer[position])
+    }
+
+    override fun getItemCount(): Int {
+        return listAnswer.size
+    }
+
+    inner class MultipleChoiceVariantViewHolder(internal val binding:CheckboxRowItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        @SuppressLint("NotifyDataSetChanged")
+        fun bind(answerData: Answer) {
+            with(binding.checkbox) {
+                text = answerData.answer
+                setOnCheckedChangeListener { _, isChecked ->
+                    if (isChecked) {
+                        selectedItems += adapterPosition
+                    } else {
+                        selectedItems = selectedItems.filter { it != adapterPosition }.toIntArray()
+                    }
+                }
+            }
+        }
+    }
+}
